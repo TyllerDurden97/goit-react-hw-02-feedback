2767b4ebab6d1a39f7b5a654d2fbef18f86ec693
TyllerDurden97 <albatros97dn@gmail.com>
2023-05-11 17:05:34 +0300

    +css

--- src/components/NotificationMessage/NotificationMessage.jsx
+++ src/components/NotificationMessage/NotificationMessage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import css from 'components/NotificationMessage/NotificationMessage.module.css';
 
 
 export const Notification = ({message }) => {
    return (
       <>
-         {message && <h2 >{message}</h2>}
+         {message && <h2 className={css.messageTitle}>{message}</h2>}
       </>
    )
 }
